385fa9340c835e2a61b01d9abb6e8c40fa043cff
elbincodes <elbincodes@gmail.com>
2017-09-07 11:18:12 -0400

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,26 @@
-class CreateStudents < ActiveRecord::Migration
+class CreateStudents < ActiveRecord::Migration[4.2]
+
+  def change
+      create_table :students do |t|
+        t.string :name
+      end
+
+  end
+
+
 end
+
+
+
+
+#
+# class CreateArtists < ActiveRecord::Migration
+#   def change
+#     create_table :artists do |t|
+#       t.string :name
+#       t.string :genre
+#       t.integer :age
+#       t.string :hometown
+#     end
+#   end
+# end
